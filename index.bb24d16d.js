2cc9fbca0def1364aff1d03f65b63c102b4461fe
Stas <staskapica93@gmail.com>
2024-08-28 18:56:47 +0200

    make build

---
+++ index.bb24d16d.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("img"),e=document.querySelector("div"),o=t.offsetHeight,f=t.offsetWidth,c=(e.offsetHeight-o)/2,i=(e.offsetWidth-f)/2;t.style.top="".concat(c,"px"),t.style.left="".concat(i,"px");
+//# sourceMappingURL=index.bb24d16d.js.map
